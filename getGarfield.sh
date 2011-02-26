8c78d9bc000c5762042b7c8cfed96cd7c895e0d7
Bastian Therres <bastian@therres.de>
2011-02-26 22:35:15 +0100

    added if-file-exists and parameter check

--- getGarfield.sh
+++ getGarfield.sh
@@ -4,10 +4,26 @@
 #FOLDERTOSAVE=$2
 COUNTER=0
 
+if [ $1 -eq $1 2> /dev/null ]; then
+    echo Loading last $1 strips...
+else
+    echo ERROR: $1 is not a number
+    exit
+fi
+
+if [ ! -d "$2" ]; then
+    echo ERROR: $2 is not a directory
+    exit
+fi
 
 while [ ${COUNTER} -le $1 ]
         do
         URL=http://images.ucomics.com/comics/ga/$(date -v-${COUNTER}d +%Y)/ga$(date -v-${COUNTER}d +%y%m%d).gif
-        curl $URL -o $2/ga$(date -v-${COUNTER}d +%y%m%d).gif        
+        
+        if [ ! -f $2/ga$(date -v-${COUNTER}d +%y%m%d).gif ]
+        then
+            curl -f $URL -o $2/ga$(date -v-${COUNTER}d +%y%m%d).gif
+        fi
+        
         COUNTER=`expr ${COUNTER} + 1`
 done
