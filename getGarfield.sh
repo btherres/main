e867a5b83532c56a13f2626bd32bcadcdfb7b7ee
Bastian Therres <bastian@therres.de>
2011-02-27 11:22:10 +0100

    added comments and made little changes

--- getGarfield.sh
+++ getGarfield.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# 2011 by Bastian Therres
+# licensed under CC-BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0/)
+#
+# Garfield by Jim Davis - (c) 2011 Paws, Inc.
+# Daily Strip at http://www.gocomics.com/garfield/
+# 
+# Description: This is a small script to download the last X comic strips of the daily garfield comic via curl
+# Usage: getGarfield.sh [number of days] [/path/to/downloadfolder/]
+
 #RANGEOFDAYS=$1
 #FOLDERTOSAVE=$2
 COUNTER=0
@@ -19,11 +28,8 @@ fi
 while [ ${COUNTER} -le $1 ]
         do
         URL=http://images.ucomics.com/comics/ga/$(date -v-${COUNTER}d +%Y)/ga$(date -v-${COUNTER}d +%y%m%d).gif
-        
-        if [ ! -f $2/ga$(date -v-${COUNTER}d +%y%m%d).gif ]
-        then
-            curl -f $URL -o $2/ga$(date -v-${COUNTER}d +%y%m%d).gif
+        if [ ! -f $2/ga$(date -v-${COUNTER}d +%y%m%d).gif ]; then
+            curl -# -f $URL -o $2/ga$(date -v-${COUNTER}d +%y%m%d).gif
         fi
-        
         COUNTER=`expr ${COUNTER} + 1`
 done
